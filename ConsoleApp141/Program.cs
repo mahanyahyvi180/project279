22192d565b4e18ce97a3b3b9705a57182d64fe0b
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-15 10:24:45 +0330

    Add files via upload

---
+++ ConsoleApp141/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp141
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception exceptionParameter)
+            {
+                Console.WriteLine("exceptionParameter.ToString:\n" + exceptionParameter);
+                Console.WriteLine("exceptionParameter.Message:\n" + exceptionParameter.Message);
+                Console.WriteLine("exceptionParameter.StackTrace:\n" + exceptionParameter.StackTrace);
+                Console.WriteLine("exceptionParameter.innerException\n" + exceptionParameter.InnerException);
+                Console.ReadLine();
+            }
+        }
+        static void Method1()
+        {
+            Method2();
+        }
+        static void Method2()
+        {
+            Method3();
+        }
+        static void Method3()
+        {
+            try
+            {
+                int.Parse("int not to press");
+            }
+            catch (FormatException formatExceptionParameter)
+            {
+                throw new Exception ("Exception",formatExceptionParameter);
+            }
+            Console.ReadLine();
+
+        }
+    }
+    }
